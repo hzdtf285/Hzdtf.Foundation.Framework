a66d50ac0a3592d95cbf0213aff01085044dc3fe
huangandy <hzdtf285@126.com>
2020-05-03 23:55:18 +0800

    添加本地系统时钟

---
+++ Common/Hzdtf.Utility.Standard/SystemV2/LocalSystemClock.cs
@@ -0,0 +1,21 @@
+﻿using Hzdtf.Utility.Standard.Attr;
+using Microsoft.Extensions.Internal;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hzdtf.Utility.Standard.SystemV2
+{
+    /// <summary>
+    /// 本地系统时钟
+    /// @ 黄振东
+    /// </summary>
+    [Inject]
+    public class LocalSystemClock : ISystemClock
+    {
+        /// <summary>
+        /// UTC当前时间
+        /// </summary>
+        public DateTimeOffset UtcNow => DateTime.Now;
+    }
+}
