21f6f98e6474f6fc0ad439571579fb580047fe6c
huangandy <hzdtf285@126.com>
2020-06-03 17:20:42 +0800

    添加linq扩展条件表达式

---
+++ Common/Hzdtf.Utility.Standard/Expressions/PredicateBuilder.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Linq;
+
+namespace Hzdtf.Utility.Standard.Expressions
+{
+    /// <summary>
+    /// 条件生成器
+    /// @ 黄振东
+    /// </summary>
+    public static class PredicateBuilder
+    {
+        /// <summary>
+        /// True表达式
+        /// </summary>
+        /// <typeparam name="T">类型</typeparam>
+        /// <returns>True表达式</returns>
+        public static Expression<Func<T, bool>> True<T>() { return f => true; }
+
+        /// <summary>
+        /// False表达式
+        /// </summary>
+        /// <typeparam name="T">类型</typeparam>
+        /// <returns>False表达式</returns>
+        public static Expression<Func<T, bool>> False<T>() { return f => false; }
+
+        /// <summary>
+        /// 组合Or表达式
+        /// </summary>
+        /// <typeparam name="T">类型</typeparam>
+        /// <param name="expr1">表达式1</param>
+        /// <param name="expr2">表达式2</param>
+        /// <returns>组合Or后的表达式</returns>
+        public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> expr1, Expression<Func<T, bool>> expr2)
+        {
+            var invokedExpr = Expression.Invoke(expr2, expr1.Parameters.Cast<Expression>());
+            return Expression.Lambda<Func<T, bool>>
+                  (Expression.Or(expr1.Body, invokedExpr), expr1.Parameters);
+        }
+
+        /// <summary>
+        /// 组合And表达式
+        /// </summary>
+        /// <typeparam name="T">类型</typeparam>
+        /// <param name="expr1">表达式1</param>
+        /// <param name="expr2">表达式2</param>
+        /// <returns>组合And后的表达式</returns>
+        public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> expr1, Expression<Func<T, bool>> expr2)
+        {
+            var invokedExpr = Expression.Invoke(expr2, expr1.Parameters.Cast<Expression>());
+            return Expression.Lambda<Func<T, bool>>
+                  (Expression.And(expr1.Body, invokedExpr), expr1.Parameters);
+        }
+    }
+}
